5a32bfbe320745c2c5d9eedf17b55042279b1689
dugganbt <99730314+dugganbt@users.noreply.github.com>
2024-09-07 11:12:14 +0200

    R-1.2 added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // R-1.1 Exercise
-        InputAllBaseTypes.inputAllBaseTypes();
+        // InputAllBaseTypes.inputAllBaseTypes();
 
 
     }
